63208f781311b95f648bf0e087299b73e39059c5
bumik94 <vaclav.himl@gmail.com>
2024-07-07 13:40:20 +0200

    Implemented menu and iterator

--- src/main/java/Places.java
+++ src/main/java/Places.java
@@ -1,34 +1,83 @@
+import java.nio.charset.StandardCharsets;
 import java.util.LinkedList;
-import java.util.Iterator;
+import java.util.ListIterator;
 import java.util.Collections;
 import java.lang.Comparable;
 import java.util.Scanner;
 
 public class Places {
-    private LinkedList<Town> list = new LinkedList<>();
+    private final LinkedList<Town> list = new LinkedList<>();
 
     public Places() {
+
         this.list.add(new Town("Adelaide", 1374));
         this.list.add(new Town("Alice Springs", 2771));
         this.list.add(new Town("Brisbane", 917));
         this.list.add(new Town("Darwin", 3972));
         this.list.add(new Town("Melbourne", 877));
         this.list.add(new Town("Perth", 3923));
+        this.list.add(new Town("Sydney", 0));
+        Collections.sort(list);
     }
 
-    public void printList() {
-        Collections.sort(list);
+    private void printList() {
+
         for (Town town : list) {
             //System.out.println(town.getName() + " " + town.getDistance());
-            System.out.printf("%s20", town.getName());
-            
+            System.out.printf("%-15s %4d%n", town.getName(), town.getDistance());
+            System.out.println("-".repeat(20));
         }
+    }
+
+    public void menu() {
+         String selection;
+        boolean      quit = false;
+            var  iterator = list.listIterator(1);
+         String printMenu = """
+                Available actions (select word or letter)
+                (F)orward
+                (B)ackwards
+                (L)ist places
+                (M)enu
+                (Q)uit""";
+
+        System.out.println(printMenu);
+        do {do { selection = getString(); } while (selection.isEmpty());
+            switch (selection.toLowerCase().charAt(0)) {
+                case ('f') -> {
+                    if (iterator.hasNext()) {
+                        var nextTown = iterator.next();
+                        System.out.printf("%s (%d km away from Sydney)%n", nextTown.getName(), nextTown.getDistance());
+                    } else {
+                        System.out.println("You reached the end");
+                    }
+                }
+                case ('b') -> {
+                    if (iterator.hasPrevious()) {
+                        var previousTown = iterator.previous();
+                        System.out.printf("%s (%d km away from Sydney)%n", previousTown.getName(), previousTown.getDistance());
+                    } else {
+                        System.out.println("You're at the beginning");
+                    }
+                }
+                case ('l') -> { printList(); }
+                case ('m') -> { System.out.println(printMenu); }
+                case ('q') -> { quit = true; }
+            }
+        } while (!quit);
+    }
+
+    private static String getString() {
+        var s = new Scanner(System.in);
+
+        System.out.print("# ");
+        return s.nextLine();
     }
 }
 
 class Town implements Comparable<Town>{
-    private  String name;
-    private Integer distance; // Comparable doesn't work on primitive data types
+    private final String name;
+    private final Integer distance; // Comparable doesn't work on primitive data types
 
     public Town(String name, int distance) {
         this.name = name;
